69a59a639192270a5fb728ff2de37fde7ef62439
Moahmmed <mohammed.serd44@gmial.com>
2022-12-15 09:14:40 +0200

    add all files

---
+++ js/about.77f72cf4.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvuetest22"]=self["webpackChunkvuetest22"]||[]).push([[443],{399:function(t,u,e){e.r(u),e.d(u,{default:function(){return i}});var n=function(){var t=this;t._self._c;return t._m(0)},s=[function(){var t=this,u=t._self._c;return u("div",{staticClass:"about"},[u("h1",[t._v("This is an about page")])])}],a=e(1),l={},r=(0,a.Z)(l,n,s,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.77f72cf4.js.map
